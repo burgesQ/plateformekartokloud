f33c394bf5e5044fc21ddf06a12fefd5efb29c5d
burgesQ <qutn.burgess@gmail.com>
2018-03-14 12:43:21 +0100

    add: cmd to update the daily karto vm

---
+++ src/Command/JsonToDatabaseCommand.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Command\Command;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Finder\Finder;
+use App\Entity\DailyKartoVm;
+
+class JsonToDatabaseCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'app:kartokloud:update';
+
+    /**
+     * @var array
+     */
+    protected static $toIgnore = ['.', '..'];
+
+    /**
+     * @var string
+     */
+    protected static $basePath = "jsonData/output";
+
+    /**
+     * @var string
+     */
+    private $rootDir;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * JsonToDatabaseCommand constructor.
+     *
+     * @param                        $kernel_root_dir
+     * @param EntityManagerInterface $em
+     * @param null|string            $name
+     */
+    public function __construct($kernel_root_dir, EntityManagerInterface $em, ?string $name = null)
+    {
+        parent::__construct($name);
+
+        $this->rootDir = $kernel_root_dir;
+        $this->em      = $em;
+    }
+
+    /**
+     * Configure the command
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Scrap the json to update the data in db.')
+            // specific dlInterface Tags
+            // specific users
+            // specific base directory
+        ;
+    }
+
+    /**
+     * @param string $data
+     *
+     * @return mixed
+     */
+    private function epur_json(string $path)
+    {
+        $jsonData   = file_get_contents($path);
+        $jsonData   = str_replace("'", '"', $jsonData);
+        $jsonData   = str_replace(" ", '', $jsonData);
+
+        return json_decode($jsonData, true);
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) : void
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $path = $this->rootDir . '/../' . self::$basePath;
+        $io->section("Let check the {$path} directory");
+
+        $finder = new Finder();
+        $finder->files()->in($path);
+
+        foreach ($finder as $file) {
+            $io->section("Process of {$file->getFilename()} ({$file->getRealPath()})");
+
+
+            $usableJson = $this->epur_json($file->getRealPath());
+
+            if (key_exists("cpu", $usableJson)) {
+
+                if (!($dailyKartoVm = $this->em->getRepository(DailyKartoVm::class)
+                                             ->findOneBy(["uniqueId" => $usableJson["name"]])))
+                    $dailyKartoVm = new DailyKartoVm();
+
+                $dailyKartoVm
+                    ->setProvider($usableJson["provider"])
+                    ->setRegion($usableJson["region"])
+                    ->setCost($usableJson["cost"])
+                    ->setRam($usableJson["nb_ram"])
+                    ->setOs($usableJson["cost"])
+                    ->setUniqueId($usableJson["name"])
+                    ->setType($usableJson["type"])
+                ;
+
+                $this->em->persist($dailyKartoVm);
+            }
+        }
+        $this->em->flush();
+    }
+}
---
+++ src/Entity/DailyKartoVm.php
@@ -0,0 +1,354 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\PersistentCollection;
+use JMS\Serializer\Annotation as JMS;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class KartoVM
+ *
+ * @package App\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="daily_karto_vm")
+ * @JMS\ExclusionPolicy("all")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class DailyKartoVm
+{
+    /**
+     * @var int
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="creation_date", type="datetime", nullable=false)
+     */
+    private $creationDate;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="update_date", type="datetime", nullable=false)
+     */
+    private $updateDate;
+
+    /**
+     * @var string
+     * @ORM\Column(name="unqiue_id", type="string", nullable=false, unique=true)
+     * @JMS\Expose()
+     */
+    private $uniqueId;
+
+    /**
+     * @var string
+     * @ORM\Column(name="provider", type="string", nullable=false)
+     * @JMS\Expose()
+     */
+    private $provider;
+
+    /**
+     * @var int
+     * @ORM\Column(name="cpu", type="integer", nullable=false)
+     * @JMS\Expose()
+     */
+    private $cpu;
+
+    /**
+     * @var int
+     * @ORM\Column(name="ram", type="bigint", nullable=false)
+     * @JMS\Expose()
+     */
+    private $ram;
+
+    /**
+     * @var float
+     * @ORM\Column(name="cost", type="float", nullable=false)
+     * @JMS\Expose()
+     */
+    private $cost;
+
+    /**
+     * @var string
+     * @ORM\Column(name="region", type="string", nullable=false)
+     * @JMS\Expose()
+     */
+    private $region;
+
+    /**
+     * @var string
+     * @ORM\Column(name="type", type="string", nullable=false)
+     * @JMS\Expose()
+     */
+    private $type;
+
+    /**
+     * @var string
+     * @ORM\Column(name="os", type="string", nullable=false)
+     * @JMS\Expose()
+     */
+    private $os;
+
+    /**
+     * Company constructor.
+     */
+    public function __construct()
+    {
+        $this->creationDate = new \Datetime();
+        $this->updateDate   = new \Datetime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get CreationDate
+     *
+     * @return \DateTime
+     */
+    public function getCreationDate() : \DateTime
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * Set CreationDate
+     *
+     * @param \DateTime $creationDate
+     *
+     * @return DailyKartoVm
+     */
+    public function setCreationDate(\DateTime $creationDate) : DailyKartoVm
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get UpdateDate
+     *
+     * @return \DateTime
+     */
+    public function getUpdateDate() : \DateTime
+    {
+        return $this->updateDate;
+    }
+
+    /**
+     * Set UpdateDate
+     *
+     * @param \DateTime $updateDate
+     *
+     * @return DailyKartoVm
+     */
+    public function setUpdateDate(\DateTime $updateDate) : DailyKartoVm
+    {
+        $this->updateDate = $updateDate;
+
+        return $this;
+    }
+
+    /**
+     * Get UniqueId
+     *
+     * @return string
+     */
+    public function getUniqueId() : string
+    {
+        return $this->uniqueId;
+    }
+
+    /**
+     * Set UniqueId
+     *
+     * @param string $uniqueId
+     *
+     * @return DailyKartoVm
+     */
+    public function setUniqueId(string $uniqueId) : DailyKartoVm
+    {
+        $this->uniqueId = $uniqueId;
+
+        return $this;
+    }
+
+    /**
+     * Get Provider
+     *
+     * @return string
+     */
+    public function getProvider() : string
+    {
+        return $this->provider;
+    }
+
+    /**
+     * Set Provider
+     *
+     * @param string $provider
+     *
+     * @return DailyKartoVm
+     */
+    public function setProvider(string $provider) : DailyKartoVm
+    {
+        $this->provider = $provider;
+
+        return $this;
+    }
+
+    /**
+     * Get Cpu
+     *
+     * @return int
+     */
+    public function getCpu() : int
+    {
+        return $this->cpu;
+    }
+
+    /**
+     * Set Cpu
+     *
+     * @param int $cpu
+     *
+     * @return DailyKartoVm
+     */
+    public function setCpu(int $cpu) : DailyKartoVm
+    {
+        $this->cpu = $cpu;
+
+        return $this;
+    }
+
+    /**
+     * Get Ram
+     *
+     * @return int
+     */
+    public function getRam() : int
+    {
+        return $this->ram;
+    }
+
+    /**
+     * Set Ram
+     *
+     * @param int $ram
+     *
+     * @return DailyKartoVm
+     */
+    public function setRam(int $ram) : DailyKartoVm
+    {
+        $this->ram = $ram;
+
+        return $this;
+    }
+
+    /**
+     * Get Cost
+     *
+     * @return float
+     */
+    public function getCost() : float
+    {
+        return $this->cost;
+    }
+
+    /**
+     * Set Cost
+     *
+     * @param float $cost
+     *
+     * @return DailyKartoVm
+     */
+    public function setCost(float $cost) : DailyKartoVm
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    /**
+     * Get Region
+     *
+     * @return string
+     */
+    public function getRegion() : string
+    {
+        return $this->region;
+    }
+
+    /**
+     * Set Region
+     *
+     * @param string $region
+     *
+     * @return DailyKartoVm
+     */
+    public function setRegion(string $region) : DailyKartoVm
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    /**
+     * Get Type
+     *
+     * @return string
+     */
+    public function getType() : string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set Type
+     *
+     * @param string $type
+     *
+     * @return DailyKartoVm
+     */
+    public function setType(string $type) : DailyKartoVm
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get Os
+     *
+     * @return string
+     */
+    public function getOs() : string
+    {
+        return $this->os;
+    }
+
+    /**
+     * Set Os
+     *
+     * @param string $os
+     *
+     * @return DailyKartoVm
+     */
+    public function setOs(string $os) : DailyKartoVm
+    {
+        $this->os = $os;
+
+        return $this;
+    }
+}
