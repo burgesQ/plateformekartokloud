708d1758be871f1147d67c1acdf4b3d5fa021612
burgesQ <qutn.burgess@gmail.com>
2018-04-23 15:19:31 +0200

    add: psr logger

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
